2ee5623054c5a528799c5e332d8d3b1cf80449d6
mikel-egana-aranguren <mikel.egana.aranguren@gmail.com>
2013-11-06 18:51:28 +0100

    Tests for shannon added; installation script improved; README.md improved

--- README.md
+++ README.md
@@ -4,15 +4,30 @@ mPuma-Galaxy
 About
 -----
 
-A Galaxy implementation of the mPuma pipeline
+A re-implementation of the mPuma pipeline (http://mpuma.sourceforge.net/) for the Galaxy platform (http://galaxyproject.org/).
 
 Dependencies
 ------------
 
 R 3.n: biom, vegan,
-Rscript (at /usr/bin/Rscript in ubuntu)
+Rscript
 
 Installation
 ------------
 
-Have a look at deploy-mPuma-Galaxy.sh
+No toolshed version yet for automated installation. Unpack and execute `deploy-mPuma-Galaxy.sh` at your own risk, or install manually by moving 
+
+Tests
+-----
+
+For example to test Shannon: 
+
+`./run_functional_tests.sh -id Shannon`
+
+Workflows
+---------
+
+Funding
+-------
+
+Mikel Egaña Aranguren is funded by the Marie Curie Cofund programme (FP7) of the European Union through the UPM.
--- deploy-mPuma-Galaxy.sh
+++ deploy-mPuma-Galaxy.sh
@@ -1,14 +1,37 @@
 #!/bin/sh
 
-# This script copies and edits the necesary files to deploy mPuma tools in a galaxy server, in order to test them
+# This script copies and edits the necesary files to deploy mPuma tools in a galaxy server
+
+# Stop Galaxy
+
+# Change Galaxy path in first variable to fit your path
 
 galaxy="/home/mikel/galaxy-dist/"
+
+# Rest of the paths should be left as is
+
+galaxy_mpuma=$galaxy"tools/mPuma/"
 main_tool_conf=$galaxy"tool_conf.xml"
 tmp_tool_conf=$galaxy"tool_conf.xml.tmp"
 
-cp galaxy-dist/tools/mPuma/* $galaxy"tools/mPuma/"
+# Create mPuma directory if it doesn't exist 
+
+if [ ! -d $galaxy_mpuma ]; then
+  mkdir $galaxy_mpuma
+fi
+ 
+# Copy the necessary files 
+ 
+cp galaxy-dist/tools/mPuma/* $galaxy_mpuma
+cp galaxy-dist/test-data/* $galaxy"test-data"
+
+# Edit tool_conf: it will add the tool_conf.xml bit to the tool_confx.xml of the galaxy server you want mPuma to be installed in
+# (mPuma will appear at the end: it can also handle already existing mPuma sections, if there are at the end, if they are not it 
+# will delete everything till </toolbox>, and you will have to recover from tool_conf.xml.bk)
 
 cp $main_tool_conf $galaxy"tool_conf.xml.bk"
 cat $main_tool_conf | sed '/<section name="mPuma/,/<\/toolbox>/d' | grep -v "</toolbox>" > $tmp_tool_conf
 less galaxy-dist/tool_conf.xml >> $tmp_tool_conf
 mv $tmp_tool_conf $main_tool_conf
+
+# Start Galaxy
